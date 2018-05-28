3fd866d51cc93a84e5e65462021b278a660bbf18
hide <hide@hides.kr>
2018-05-28 18:28:20 +0900

    Nike ing...

---
+++ Nike/src.js
@@ -0,0 +1,29 @@
+/* MEN DROPDOWN */
+showMenDropdown = () => {
+    document.getElementById("nav-bottom-dropdown1").style.display = 'flex';
+}
+hideMenDropdown = () => {
+    document.getElementById("nav-bottom-dropdown1").style.display = 'none';
+}
+/* WOMEN DROPDOWN */
+showWomenDropdown = () => {
+    document.getElementById("nav-bottom-dropdown2").style.display = 'flex';
+}
+hideWomenDropdown = () => {
+    document.getElementById("nav-bottom-dropdown2").style.display = 'none';
+}
+/* BOY DROPDOWN */
+showBoyDropdown = () => {
+    document.getElementById("nav-bottom-dropdown3").style.display = 'flex';
+}
+hideBoyDropdown = () => {
+    document.getElementById("nav-bottom-dropdown3").style.display = 'none';
+}
+/* GIRLS DROPDOWN */
+showGirlsDropdown = () => {
+    document.getElementById("nav-bottom-dropdown4").style.display = 'flex';
+}
+hideGirlsDropdown = () => {
+    document.getElementById("nav-bottom-dropdown4").style.display = 'none';
+}
+/* COLLECTION DROPDOWN */
