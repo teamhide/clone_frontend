b0551c1faf84a9c2e008897ce35b5b41efc71300
hide <hide@hides.kr>
2018-05-29 20:52:45 +0900

    Tomford Mobile Search

--- Tomford/src.js
+++ Tomford/src.js
@@ -5,3 +5,10 @@ showMobileMenu = () => {
 hideMobileMenu = () => {
     document.getElementById("mobile-menu-overlay").style.display = 'none';
 }
+/* MOBILE SEARCH */
+showSearch = () => {
+    document.getElementById("mobile-search").style.display = 'flex';
+}
+hideSearch = () => {
+    document.getElementById("mobile-search").style.display = 'none';
+}
